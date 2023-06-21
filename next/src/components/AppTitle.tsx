58258d188d2289d8efcac87ec5e2c72fba626636
xiaol <liuxiao.in@gmail.com>
2023-06-21 18:51:21 +0800

    update RWKV

--- next/src/components/AppTitle.tsx
+++ next/src/components/AppTitle.tsx
@@ -10,7 +10,7 @@ const AppTitle = () => {
     <div id="title" className="relative flex flex-col items-center font-mono">
       <div className="flex flex-row items-start">
         <span className="text-4xl font-bold text-[#C0C0C0] xs:text-5xl sm:text-6xl">Agent</span>
-        <span className="text-4xl font-bold text-white xs:text-5xl sm:text-6xl">GPT</span>
+        <span className="text-4xl font-bold text-white xs:text-5xl sm:text-6xl">RWKV</span>
         <PopIn delay={0.5}>
           <Badge colorClass="bg-gradient-to-t from-sky-500 to-sky-600 border-2 border-white/20">
             {i18n?.t("BETA", {
